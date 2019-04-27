185861d4bda411a9f670bdac82beaff879e630d1
dzaima <atdzim@inbox.lv>
2019-04-27 18:32:39 +0300

    ctrl+B, dark usage menu

--- app/src/processing/app/syntax/JEditTextArea.java
+++ app/src/processing/app/syntax/JEditTextArea.java
@@ -29,6 +29,7 @@ import javax.swing.plaf.basic.*;
 import java.awt.im.InputMethodRequests;
 
 import processing.app.syntax.im.InputMethodSupport;
+import processing.app.ui.DarkScrollBarUI;
 import processing.core.PApplet;
 
 /**
@@ -186,43 +187,6 @@ public class JEditTextArea extends JPanel
     DarkJScrollBar(int typeIguess) {
       super(typeIguess);
       this.setUI(new DarkScrollBarUI());
-    }
-    public class DarkScrollBarUI extends BasicScrollBarUI {
-      
-      @Override
-      protected void paintThumb(Graphics g, JComponent c, Rectangle thumbBounds) {
-        Graphics2D g2g = (Graphics2D) g;
-        g2g.setColor(new Color(0xff444444));
-        g2g.fill(thumbBounds);
-        //super.paintThumb(g, c, thumbBounds);
-      }
-      
-      @Override
-      protected void paintTrack(Graphics g, JComponent c, Rectangle trackBounds) {
-        Graphics2D g2g = (Graphics2D) g;
-        g2g.setColor(new Color(0xff292929));
-        g2g.fill(trackBounds);
-        // super.paintTrack(g, c, trackBounds);
-        // g2g.dispose();
-      }
-      JButton createZeroButton() {
-        JButton button = new JButton("zero button");
-        Dimension zeroDim = new Dimension(0,0);
-        button.setPreferredSize(zeroDim);
-        button.setMinimumSize(zeroDim);
-        button.setMaximumSize(zeroDim);
-        return button;
-      }
-      
-      @Override
-      protected JButton createDecreaseButton(int orientation) {
-        return createZeroButton();
-      }
-      
-      @Override
-      protected JButton createIncreaseButton(int orientation) {
-        return createZeroButton();
-      }
     }
   }
 
